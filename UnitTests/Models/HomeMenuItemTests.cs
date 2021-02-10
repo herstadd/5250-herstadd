e7b42baa30276d007b24e58b67c27e95fdabae20
herstadd <herstadd@seattleu.edu>
2021-02-10 03:42:24 -0800

    Added UT for HomeMenuItem to HomeMenuItemTests.cs

---
+++ UnitTests/Models/HomeMenuItemTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Mine.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class HomeMenuItemTests
+    {
+        [Test]
+        public void HomeMenuItem_Constructor_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            //Act
+            var result = new HomeMenuItem();
+
+            //Reset
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        //Template:
+
+        // Arrange
+
+        // Act
+
+        // Reset
+
+        // Assert
+
+        [Test]
+        public void HomeMenuItem_Set_Get_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new HomeMenuItem();
+            result.Id = MenuItemType.Game;
+            result.Title = "Title";
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(result.Id, MenuItemType.Game);
+            Assert.AreEqual(result.Title, "Title");
+        }
+
+    }
+}
